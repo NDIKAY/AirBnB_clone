994c026116c8612fd03b1290bab255513ea306df
ndikubwimana Balthazar <nbarthazar80@gmail.com>
2024-04-04 11:32:28 +0200

    Airbnb

--- tests/test_console.py
+++ tests/test_console.py
@@ -108,9 +108,8 @@ class TestHBNBCommand_exit(unittest.TestCase):
     """Unittests for testing exiting from the HBNB command interpreter."""
 
     def test_quit_exits(self):
-        with patch("sys.stdout", new=StringIO()) as output:
+    with patch("sys.stdout", new=StringIO()) as output:
             self.assertTrue(HBNBCommand().onecmd("quit"))
-
     def test_EOF_exits(self):
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertTrue(HBNBCommand().onecmd("EOF"))
